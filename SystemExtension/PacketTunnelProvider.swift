9dc7713bd056fd5082ef417ea34b67f5189d8f2e
世界 <i@sekai.icu>
2023-09-20 21:16:58 +0800

    Fix iCloud profile on macos

--- SystemExtension/PacketTunnelProvider.swift
+++ SystemExtension/PacketTunnelProvider.swift
@@ -9,6 +9,7 @@ class PacketTunnelProvider: ExtensionProvider {
         }
         let username = usernameObject as! NSString
         FilePath.sharedDirectory = URL(filePath: "/Users/\(username)/Library/Group Containers/\(FilePath.groupName)")
+        FilePath.iCloudDirectory = URL(filePath: "/Users/\(username)/Library/Mobile Documents/iCloud~\(FilePath.packageName.replacingOccurrences(of: ".", with: "~"))").appendingPathComponent("Documents", isDirectory: true)
         self.username = String(username)
         try await super.startTunnel(options: options)
     }
