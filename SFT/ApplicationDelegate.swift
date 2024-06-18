aa4ce9842174af85e4d3b2bd5e1651e4f87d3526
世界 <i@sekai.icu>
2024-06-18 08:43:41 +0800

    Fix build with xcode 16 beta

--- SFT/ApplicationDelegate.swift
+++ SFT/ApplicationDelegate.swift
@@ -8,15 +8,13 @@ class ApplicationDelegate: NSObject, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         NSLog("Here I stand")
         LibboxSetup(FilePath.sharedDirectory.relativePath, FilePath.workingDirectory.relativePath, FilePath.cacheDirectory.relativePath, true)
-        Task {
-            await setup()
-        }
+        setup()
         return true
     }
 
-    private func setup() async {
+    private func setup() {
         do {
-            try await UIProfileUpdateTask.configure()
+            try UIProfileUpdateTask.configure()
             NSLog("setup background task success")
         } catch {
             NSLog("setup background task error: \(error.localizedDescription)")
