24efd1f87c76dd86182caefbabf7f6b05c7e9292
世界 <i@sekai.icu>
2025-01-28 09:20:58 +0800

    Add stub for `localDNSTransport` and `systemCertificates`

--- Library/Network/ExtensionPlatformInterface.swift
+++ Library/Network/ExtensionPlatformInterface.swift
@@ -423,4 +423,12 @@ public class ExtensionPlatformInterface: NSObject, LibboxPlatformInterfaceProtoc
             }
         #endif
     }
+    
+    public func localDNSTransport() -> (any LibboxLocalDNSTransportProtocol)? {
+        return nil
+    }
+    
+    public func systemCertificates() -> (any LibboxStringIteratorProtocol)? {
+        return nil
+    }
 }
