f341316b49036db2c05ce96b4d18175158a5ee40
世界 <i@sekai.icu>
2026-07-26 15:24:21 +0800

    Fix toolbar flickering during refresh

--- ApplicationLibrary/Views/Connections/ConnectionListViewModel.swift
+++ ApplicationLibrary/Views/Connections/ConnectionListViewModel.swift
@@ -1,99 +1,66 @@
+import Combine
 import Libbox
 import Library
 import SwiftUI
 
 @MainActor
-public class ConnectionListViewModel: BaseViewModel {
-    @Published public var connections: [Connection] = [] {
-        didSet {
-            updateFilteredConnections()
-        }
-    }
+public class ConnectionDataModel: ObservableObject {
+    @Published public private(set) var connections: [Connection] = []
+    @Published public private(set) var filteredConnections: [Connection] = []
+    @Published public private(set) var isLoading = true
 
-    @Published public var searchText = "" {
-        didSet {
-            updateFilteredConnections()
-        }
-    }
+    private let commandClient: CommandClient
+    private var searchText: String
+    private var cancellables = Set<AnyCancellable>()
 
-    @Published public var isSearching = false
+    init(commandClient: CommandClient, viewModel: ConnectionListViewModel) {
+        self.commandClient = commandClient
+        searchText = viewModel.searchText
 
-    public func toggleSearch() {
-        isSearching.toggle()
-        if !isSearching {
-            searchText = ""
-        }
-    }
-
-    @Published public var filteredConnections: [Connection] = []
-    @Published public var connectionStateFilter: ConnectionStateFilter {
-        didSet {
-            saveStateFilterTask?.cancel()
-            saveStateFilterTask = Task {
-                await SharedPreferences.connectionStateFilter.set(connectionStateFilter.rawValue)
+        viewModel.$connectionStateFilter
+            .sink { [weak self] filter in
+                guard let self else { return }
+                self.commandClient.connectionStateFilter = filter
+                self.commandClient.filterConnectionsNow()
             }
-        }
-    }
+            .store(in: &cancellables)
 
-    @Published public var connectionSort: ConnectionSort {
-        didSet {
-            saveSortTask?.cancel()
-            saveSortTask = Task {
-                await SharedPreferences.connectionSort.set(connectionSort.rawValue)
+        viewModel.$connectionSort
+            .sink { [weak self] sort in
+                guard let self else { return }
+                self.commandClient.connectionSort = sort
+                self.commandClient.filterConnectionsNow()
             }
-        }
+            .store(in: &cancellables)
+
+        viewModel.$searchText
+            .dropFirst()
+            .sink { [weak self] searchText in
+                guard let self else { return }
+                self.searchText = searchText
+                self.updateFilteredConnections()
+            }
+            .store(in: &cancellables)
+
+        commandClient.$connections
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] connections in
+                self?.setConnections(connections)
+            }
+            .store(in: &cancellables)
     }
 
-    private var connectTask: Task<Void, Never>?
-    private var saveStateFilterTask: Task<Void, Never>?
-    private var saveSortTask: Task<Void, Never>?
-
-    override public init() {
-        connectionStateFilter = .active
-        connectionSort = .byDate
-        super.init()
-        isLoading = true
-    }
-
-    public func connect() {
-        if Variant.screenshotMode {
+    func finishLoading() {
+        if isLoading {
             isLoading = false
-            return
-        }
-
-        connectTask?.cancel()
-        connectTask = Task { @MainActor [weak self] in
-            guard let self else { return }
-            await self.loadPreferences()
-            if Task.isCancelled {
-                return
-            }
-            self.connectTask = nil
         }
     }
 
-    private func loadPreferences() async {
-        let filter = await ConnectionStateFilter(rawValue: SharedPreferences.connectionStateFilter.get()) ?? .active
-        let sort = await ConnectionSort(rawValue: SharedPreferences.connectionSort.get()) ?? .byDate
-        connectionStateFilter = filter
-        connectionSort = sort
-    }
-
-    public func disconnect() {
-        connectTask?.cancel()
-        connectTask = nil
-        saveStateFilterTask?.cancel()
-        saveStateFilterTask = nil
-        saveSortTask?.cancel()
-        saveSortTask = nil
-    }
-
-    public func closeAllConnections() {
-        do {
-            try CommandTarget.standaloneClient().closeConnections()
-        } catch {
-            alert = AlertState(action: "close all connections", error: error)
-        }
+    private func setConnections(_ goConnections: [LibboxConnection]) {
+        connections = convertConnections(goConnections)
+        updateFilteredConnections()
+        finishLoading()
     }
 
     private func updateFilteredConnections() {
@@ -102,12 +69,6 @@ public class ConnectionListViewModel: BaseViewModel {
         } else {
             filteredConnections = connections.filter { $0.performSearch(searchText) }
         }
-    }
-
-    public func setConnections(_ goConnections: [LibboxConnection]?) {
-        guard let goConnections else { return }
-        connections = convertConnections(goConnections)
-        isLoading = false
     }
 
     private func convertConnections(_ goConnections: [LibboxConnection]) -> [Connection] {
@@ -146,5 +107,94 @@ public class ConnectionListViewModel: BaseViewModel {
             ))
         }
         return connections
+    }
+}
+
+@MainActor
+public class ConnectionListViewModel: BaseViewModel {
+    @Published public var searchText = ""
+    @Published public var isSearching = false
+
+    @Published public var connectionStateFilter: ConnectionStateFilter {
+        didSet {
+            saveStateFilterTask?.cancel()
+            saveStateFilterTask = Task {
+                await SharedPreferences.connectionStateFilter.set(connectionStateFilter.rawValue)
+            }
+        }
+    }
+
+    @Published public var connectionSort: ConnectionSort {
+        didSet {
+            saveSortTask?.cancel()
+            saveSortTask = Task {
+                await SharedPreferences.connectionSort.set(connectionSort.rawValue)
+            }
+        }
+    }
+
+    public let commandClient = CommandClient([.connections])
+    public private(set) var dataModel: ConnectionDataModel!
+
+    private var connectTask: Task<Void, Never>?
+    private var saveStateFilterTask: Task<Void, Never>?
+    private var saveSortTask: Task<Void, Never>?
+
+    override public init() {
+        connectionStateFilter = .active
+        connectionSort = .byDate
+        super.init()
+        dataModel = ConnectionDataModel(commandClient: commandClient, viewModel: self)
+    }
+
+    public func toggleSearch() {
+        isSearching.toggle()
+        if !isSearching {
+            searchText = ""
+        }
+    }
+
+    public func connect() {
+        commandClient.connect()
+
+        if Variant.screenshotMode {
+            dataModel.finishLoading()
+            return
+        }
+
+        connectTask?.cancel()
+        connectTask = Task { @MainActor [weak self] in
+            guard let self else { return }
+            await self.loadPreferences()
+            if Task.isCancelled {
+                return
+            }
+            self.connectTask = nil
+        }
+    }
+
+    private func loadPreferences() async {
+        let filter = await ConnectionStateFilter(rawValue: SharedPreferences.connectionStateFilter.get()) ?? .active
+        let sort = await ConnectionSort(rawValue: SharedPreferences.connectionSort.get()) ?? .byDate
+        connectionStateFilter = filter
+        connectionSort = sort
+    }
+
+    public func disconnect() {
+        commandClient.disconnect()
+        connectTask?.cancel()
+        connectTask = nil
+        saveStateFilterTask?.cancel()
+        saveStateFilterTask = nil
+        saveSortTask?.cancel()
+        saveSortTask = nil
+    }
+
+    public func closeAllConnections() {
+        do {
+            try CommandTarget.standaloneClient().closeConnections()
+        } catch {
+            alert = AlertState(action: "close all connections", error: error)
+        }
     }
 }
